7f024ca17fb3523a13b61fb372ee34c5a2e97798
Offolmotopia <74813567+Offolmotopia@users.noreply.github.com>
2021-04-27 17:21:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,106 @@
 // Copyright 2021 NNTU-CS
 #include <string>
-#include "tstack.h"
-
+#include <map>
+#include <stack>
 std::string infx2pstfx(std::string inf) {
-  // добавьте сюда нужный код
+    std::map<char, int> priorities = { {'(', 0}, {')', 1}, {'+', 2}, {'-', 2}, {'*', 3}, {'/', 3} };
+    std::string resString, temp;
+    std::stack<char> stack;
+    for (auto& current : inf) {
+        if (current >= '0' && current <= '9') {
+            temp += current;
+        } else if (current == '(' || current == '+' ||
+            current == '-' || current == '*' || current == '/') {
+            if (temp.length()) {
+                resString += temp;
+                resString += ' ';
+                temp = "";
+            }
+            if (!stack.empty()) {
+                if (current == '(') {
+                    stack.push(current);
+                } else if (priorities[current] > priorities[stack.top()]) {
+                    stack.push(current);
+                } else {
+                    resString += stack.top();
+                    stack.pop();
+                    stack.push(current);
+                    resString += ' ';
+                }
+            } else {
+                stack.push(current);
+            }
+        } else if (current == ')') {
+            if (temp.length()) {
+                resString += temp;
+                resString += ' ';
+                temp = "";
+            }
+            if (!stack.empty()) {
+                while (stack.top() != '(') {
+                    resString += stack.top();
+                    stack.pop();
+                    resString += ' ';
+                }
+                stack.pop();
+            }
+        }
+    }
+    if (temp.length()) {
+        resString += temp;
+        resString += ' ';
+   }
+    for (unsigned int i = stack.size(); i > 0; i--) {
+        resString += stack.top();
+        resString += ' ';
+        stack.pop();
+    }
+    resString.erase(resString.end() - 1, resString.end());
+    return resString;
   return std::string("");
 }
 
 int eval(std::string pst) {
-  // добавьте сюда нужный код
-  return 0;
+    unsigned int x = 0, y = 0;
+    std::stack<unsigned int> stack;
+    unsigned int temp = 0;
+    for (unsigned int i = 0; i < pst.length() + 1; i++) {
+        if (pst[i] >= '0' && pst[i] <= '9') {
+            temp *= 10;
+            temp += pst[i] - '0';
+        } else if (pst[i] == ' ') {
+            stack.push(temp);
+            temp = 0;
+        } else {
+            x = stack.top();
+            stack.pop();
+            y = stack.top();
+            stack.pop();
+            switch (pst[i])
+            {
+            case ('+'):
+                stack.push(y + x);
+                break;
+            case ('-'):
+                stack.push(y - x);
+                break;
+            case ('*'):
+                stack.push(y * x);
+                break;
+            case ('/'):
+                stack.push(y / x);
+                break;
+            default:
+                break;
+            }
+            i++;
+        }
+    }
+    return stack.top();
+}
+
+int main() {
+    std::cout << infx2pstfx("2*(3-6+4*(2-1))/6+3/2") << std::endl;
+    std::cout << eval(infx2pstfx("2*(3-6+4*(2-1))/6+3/2")) << std::endl;
+    return 0;
 }
